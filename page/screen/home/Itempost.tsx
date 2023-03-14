dc307a5c02e9c2891fd9a0a939fd6308c30276ee
Bro-GunHo <redwizzaa@naver.com>
2023-03-14 11:02:53 +0900

    error

--- page/screen/home/Itempost.tsx
+++ page/screen/home/Itempost.tsx
@@ -53,7 +53,7 @@ const Itempost = ({ route }: Props) => {
   const userInfo = useSelector((state: any) => state.userInfo);
   const navigation = useNavigation<StackNavigationProp<MainNavigatorParams>>();
   const myLocation = useSelector((state: any) => state.myLocation);
-  const { t,i18n } = useTranslation();
+  const { t, i18n } = useTranslation();
 
   if (!route?.params?.pt_idx) {
     cusToast(t('잘못된 방법입니다'));
@@ -207,53 +207,55 @@ const Itempost = ({ route }: Props) => {
     //   cusToast(t('이미 거래가 완료된 상품입니다.'));
     //   return false;
     // }
-    if(myLocation.select_location == 1 && myLocation.location1.mat_status !== "Y" ){
+    if (myLocation.select_location == 1 && myLocation.location1.mat_status !== "Y") {
       Certified()
-    } else if(myLocation.select_location == 2 && myLocation.location2.mat_status !== "Y"){
+    } else if (myLocation.select_location == 2 && myLocation.location2.mat_status !== "Y") {
       Certified()
     } else {
       if (userInfo.idx == items.data[0].mt_seller_idx) {
         //채팅응답
         navigation.navigate('Reserve_choice', { target, type: 'reserveChat' });
       } else {
-      await client({
-        method: 'post',
-        url: '/product/chat_add',
-        data: {
-          mt_idx: userInfo.idx,
-          pt_idx: pt_idx,
-        },
-      })
-        .then(res => {
-          //{"crt_idx": 52, "ctt_room_id": "CzAuaGg4fxlXg"}
-          if (res.data.crt_idx) {
-            cusToast(t('판매자에게 채팅요청을 보냈습니다.'))
-          }
-          //console.log(res.data);
+        await client({
+          method: 'post',
+          url: '/product/chat_add',
+          data: {
+            mt_idx: userInfo.idx,
+            pt_idx: pt_idx,
+          },
         })
-        .catch(error => {
-          console.log(error);
-        });
-    }}
+          .then(res => {
+            //{"crt_idx": 52, "ctt_room_id": "CzAuaGg4fxlXg"}
+            if (res.data.crt_idx) {
+              cusToast(t('판매자에게 채팅요청을 보냈습니다.'))
+            }
+            //console.log(res.data);
+          })
+          .catch(error => {
+            console.log(error);
+          });
+      }
+    }
   };
 
-  const Certified = () =>{
-    Alert.alert(t('현재동네 인증이 되어있지않습니다'),'',
+  const Certified = () => {
+    Alert.alert(t('현재동네 인증이 되어있지않습니다'), '',
       [
-        {text:t('인증하기'), onPress:()=>{
-          navigation.navigate('SetMyLocation')
-        }},
         {
-          text:t('취소'),onPress:()=>{}
+          text: t('인증하기'), onPress: () => {
+            navigation.navigate('SetMyLocation')
+          }
+        },
+        {
+          text: t('취소'), onPress: () => { }
         }
       ])
   }
 
   /** 상품 정보 가져오기 ${route.params.pt_idx}*/
-  React.useEffect(() => {
-    const subscribe = navigation.addListener('focus', () => {
-      getPostData();
-    });
+  /** 상품 정보 가져오기 ${route.params.pt_idx} 상품상세에서 인텐트를 받아도 이동하게 수정 */
+  useFocusEffect(React.useCallback(() => {
+    getPostData();
 
   }, [pt_idx]))
 
